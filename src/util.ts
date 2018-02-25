3b394928f412ba644153acb86fae7a1ef2f072dc
Adam Some <adamsome@gmail.com>
2018-02-25 02:23:04 -0500

    Remove unused ramda package

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import { clamp, curry } from 'ramda'
 
 const JSON_EXT = '.json'
 
@@ -18,23 +17,20 @@ export function writeJson(filePath: string, valueOrFileName: any, value?: any) {
 export const readJson = (filePath: string): any =>
   JSON.parse(fs.readFileSync(path.resolve(filePath), 'utf8'))
 
-export const opacity: {
-  (percent: number, color: string): string
-  (percent: number): (color: string) => string
-} = curry((percent: number, color: string) => {
+export const opacity = (percent: number, color: string) => {
   // Convert percent to range from 0 - 255
-  const percent256 = clamp(0, 255, Math.round(percent * 256 / 100))
+  let percent256 = Math.round(percent * 256 / 100)
+  percent256 = percent256 < 0 ? 0 : percent256 > 255 ? 255 : percent256
+  // Convert to hex string
   const opacityHex = percent256.toString(16)
+  // Append, padding left w/ zeroes
   return color + padZeroes(2, opacityHex)
-})
+}
 
-export const padZeroes: {
-  (size: number, value: number | string): string
-  (size: number): (value: number | string) => string
-} = curry((size: number, value: number | string) => {
+export const padZeroes = (size: number, value: number | string) => {
   let _value = value.toString()
   while (_value.length < size) {
     _value = '0' + _value
   }
   return _value
-})
+}
